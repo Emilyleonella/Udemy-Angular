f34554be25b5cbcaee787ba8a70e6e125110d2f1
Emily Leonella <emilytamayo@EmilyLeonella.lan>
2022-05-23 12:12:30 -0400

    started to work with Property Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,10 @@ import { Component } from "@angular/core";
 })
 export class ServerComponent{
 
+    serverId: number = 10; // you can write number infront to let you know what type of data it is
+    serverStatus: string = 'offline'
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
