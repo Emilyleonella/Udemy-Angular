3f6cbc2739d71fbce4f7ac9e0dc21011ab150391
Emily Leonella <emilytamayo@EmilyLeonella.lan>
2022-05-31 14:44:12 -0400

    worked on applying CSS classes dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { withLatestFrom } from "rxjs";
 
 @Component({
     selector:'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .Online{
+        color: white;
+    }
+    `]
 
 })
 export class ServerComponent{
@@ -10,7 +16,14 @@ export class ServerComponent{
     serverId: number = 10; // you can write number infront to let you know what type of data it is
     serverStatus: string = 'offline'
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline'
+    }
+
     getServerStatus(){
         return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus === 'Online' ? 'green' : 'red'
     }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
 
 @Component({
   //selector: '[app-servers]',
@@ -13,6 +14,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+  serverName = 'TestServer'
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() { 
     setTimeout(()=> {
@@ -23,4 +28,15 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+onCreateServer(){
+  this.serverCreated = true;
+  this.servers.push(this.serverName);
+  this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
 }
+onUpdateServerName(event: Event){
+this.serverName = (<HTMLInputElement>event.target).value
+// console.log(this.serverName)
+}
+
+}
+
